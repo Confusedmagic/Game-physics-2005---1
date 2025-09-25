748fd7e3e43a51becdba09f87f078e0ed0cd23f1
JossKM <31990727+JossKM@users.noreply.github.com>
2025-09-25 13:57:51 -0400

    Added projectile spawning!

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -8,17 +8,67 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 #include "game.h"
+#include <string>
+#include <vector>
 
 const unsigned int TARGET_FPS = 50; //frames/second
 float dt = 1.0f / TARGET_FPS; //seconds/frame
 float time = 0;
 
+class FizziksObjekt
+{
+public:
+	Vector2 position = { 0,0 };
+	Vector2 velocity = { 0,0 };
+	float mass = 1; // in kg
+
+	float radius = 15; // circle radius in pixels
+	std::string name = "objekt";
+	Color color = RED;
+
+	void draw()
+	{
+		DrawCircle(position.x, position.y, radius, color);
+
+		DrawText(name.c_str(), position.x, position.y, radius * 2, LIGHTGRAY);
+
+		//Draw velocity (for fun)
+		DrawLineEx(position, position + velocity, 1, color);
+	}
+};
+
+class FizziksWorld
+{
+private:
+	unsigned int objektCount = 0;
+public: 
+	std::vector<FizziksObjekt> objekts; // All objects in physics simulation
+	Vector2 accelerationGravity = {0, 9};
+
+	void add(FizziksObjekt newObject) // Add to physics simulation
+	{
+		newObject.name = std::to_string(objektCount);
+		objekts.push_back(newObject);
+		objektCount++;
+	}
+
+	// Update state of all physics objects
+	void update()
+	{
+		for (int i = 0; i < objekts.size(); i++)
+		{
+			//vel = change in position / time, therefore     change in position = vel * time 
+			objekts[i].position = objekts[i].position + objekts[i].velocity * dt;
+			//accel = deltaV / time (change in velocity over time) therefore     deltaV = accel * time
+			objekts[i].velocity = objekts[i].velocity + accelerationGravity * dt;
+		}
+	}
+};
+
 float speed = 100;
 float angle = 0;
 
-Vector2 position = { 500, 500 };
-Vector2 velocity = { 0,0 };
-Vector2 accelerationGravity = {0, 9};
+FizziksWorld world;
 
 //Changes world state
 void update()
@@ -26,17 +76,20 @@ void update()
 	dt = 1.0f / TARGET_FPS;
 	time += dt;
 
-
-	//vel = change in position / time, therefore     change in position = vel * time 
-	position = position + velocity * dt;
-
-	//accel = deltaV / time (change in velocity over time) therefore     deltaV = accel * time
-	velocity = velocity + accelerationGravity * dt;
+	world.update();
 
 	if (IsKeyPressed(KEY_SPACE))
 	{
-		position = { 100, (float)GetScreenHeight() - 100 };
-		velocity = { speed * (float)cos(angle * DEG2RAD), -speed * (float)sin(angle * DEG2RAD) };
+		FizziksObjekt newBird;
+		newBird.position = { 100, (float)GetScreenHeight() - 100 };
+		newBird.velocity = { speed * (float)cos(angle * DEG2RAD), -speed * (float)sin(angle * DEG2RAD) };
+		
+		//rand() % N produces random number from 0 to N-1
+		newBird.radius = (rand() % 26) + 5; // radius from 5-30
+		Color randomColor = {rand() % 256 , rand() % 256, rand() % 256, 255};
+		newBird.color = randomColor;
+
+		world.add(newBird); // Add bird to simulation
 	}
 }
 
@@ -54,7 +107,7 @@ void draw()
 
 	GuiSliderBar(Rectangle{ 10, 80, 500, 30 }, "Angle", TextFormat("Angle: %.0f Degrees", angle), &angle, -180, 180);
 
-	GuiSliderBar(Rectangle{ 10, 120, 500, 30 }, "Gravity Y", TextFormat("Gravity Y: %.0f Px/sec^2", accelerationGravity.y), &accelerationGravity.y, -1000, 1000);
+	GuiSliderBar(Rectangle{ 10, 120, 500, 30 }, "Gravity Y", TextFormat("Gravity Y: %.0f Px/sec^2", world.accelerationGravity.y), &world.accelerationGravity.y, -1000, 1000);
 
 
 
@@ -65,7 +118,11 @@ void draw()
 
 	DrawLineEx(startPos, startPos + velocity, 3, RED);
 
-	DrawCircle(position.x, position.y, 15, RED);
+	//Draw all physics objects!
+	for (int i = 0; i < world.objekts.size(); i++)
+	{
+		world.objekts[i].draw();
+	}
 
 	EndDrawing();
 
